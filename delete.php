496064e5e03c6b19ac43a5e75045bfc41f2e5b76
DESKTOP-6GENN4I\satas <satasai@jcom.home.ne.jp>
2018-01-27 19:52:16 +0900

    新規に授業で作成したファイルを保存します。

---
+++ delete.php
@@ -0,0 +1,38 @@
+<?php
+//index.php（登録フォームの画面ソースコードを全コピーして、このファイルをまるっと上書き保存）
+include("functions.php");
+$id = $_GET["id"];
+
+// echo "GET:".$id;
+
+//1.  DB接続します
+$pdo=db_con();
+
+
+  //２．データ登録SQL作成
+  $stmt = $pdo->prepare("DELETE  FROM gs_an_table WHERE id=:id");
+  $stmt->bindvalue(":id",$id, PDO::PARAM_INT);
+  $status = $stmt->execute();
+  
+  
+  //３．データ表示
+  $view="";
+  if($status==false){
+    //execute（SQL実行時にエラーがある場合）
+    error_db_Info($stmt);    
+
+  }else{
+      header("Location: select.php");
+      exit();
+    //    <!-- $row= $stmt->fetch(); -->
+    //Selectデータの数だけ自動でループしてくれる
+    // while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+    //   $view .='<p>';
+    //   $view .='<a href="detail.php?id='.$result["id"].'">'; 
+    //   $view .= $result["name"]."[".$result["indate"]."]";
+    //   $view .='</a>';
+    //   $view .='</p>';
+    // }
+  }
+
+  ?>
---
+++ test.sql
@@ -0,0 +1,3 @@
+UPDATE gs_an_table SET
+name='TEST1',email='test@test.com',naiyou='naiyou1'
+WHERE id=6
